72d6cb5ac8672717aa86f7925dea42fc67a4e6ab
Tamilselvan R <ranganathanselvan@gmail.com>
2021-12-14 19:11:49 +0530

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ManageUserComponent } from './admin/manage-user/manage-user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ManageUserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
